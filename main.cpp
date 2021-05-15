ec113fd73a06f8bc61f73bf369c9691ba70e1716
Bob (NikitinaNK) <iiam_nikitina@icloud.com>
2021-05-15 13:51:50 +0300

    code: min

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main()
 {cout << "Enter A and B: ";
-int a, b;
+int a, b, min;
 cin >> a >> b;
  cout << "A + B = " << a + b << '\n'
       << "A - B = " << a - b << '\n'
       << "A*B = " << a*b << '\n'
       << "A / B = " << a / b << '\n';
+      min=b;
+      if(a<b)
+      {
+          min=a;
+          cout << "min = " << a << '\n';
+      }
+      else
+        cout << "min= " << b << '\n';
     return 0;
 }
