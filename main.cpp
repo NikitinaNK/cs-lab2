3b1937f621baa04b84cb2ef32537c37427e10efb
Alice (NikitinaNK) <iiam_nikitina@icloud.com>
2021-05-15 13:44:29 +0300

    code: max

--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main()
 {
 cout << "Enter A and B: ";
-int a, b;
+int a, b,max;
 cin >> a >> b;
+max=a;
+      if (a<b)
+      {
+          max=b;
+          cout << "max= " << b << '\n';
+      }
+      else
+        cout << "max= " << a << '\n';
  cout << "A + B = " << a + b << '\n'
       << "A - B = " << a - b << '\n'
       << "A*B = " << a*b << '\n'
